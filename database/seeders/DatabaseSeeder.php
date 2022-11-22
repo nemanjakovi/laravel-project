649ebab3a700d8d78838b636e25c4e5fd800c479
nemanjakovi <nemanjakovi90@gmail.com>
2022-11-22 16:18:22 +0100

    Database Migrations, Seeders, Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Junior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Crop',
+        //     'location' => 'Belgrade',
+        //     'email' => 'mojmail@gmail.com',
+        //     'website' => 'https://www.mojsajt.com',
+        //     'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        //     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        //     when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full_Stack Engineer',
+        //     'tags' => 'laravel, jbackend,api',
+        //     'company' => 'Bomo Crop',
+        //     'location' => 'Belgrade',
+        //     'email' => 'mojmail@gmail.com',
+        //     'website' => 'https://www.mysecundsite.com',
+        //     'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        //     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        //     when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,6 +7,6 @@
 <h2>
      <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
     </h2>
-    <p>{{ $listing['description'] }}</p>
+    <p>{{ $listing['description']}}</p>
    
 @endforeach
