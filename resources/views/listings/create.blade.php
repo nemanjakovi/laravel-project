1e88f60cb566b74f0afc61b03a6efed89e12b9dc
nemanjakovi <nemanjakovi90@gmail.com>
2022-11-28 14:45:25 +0100

    Keep Old Data In Form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,10 +18,10 @@
                                 class="inline-block text-lg mb-2"
                                 >Company Name</label
                             >
-                            <input
+                            <input class="border border-gray-200 rounded p-2 w-full"
                                 type="text"
-                                class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value="{{ old('company') }}"
                             />
 
                             @error('company')
@@ -34,11 +34,12 @@
                             <label for="title" class="inline-block text-lg mb-2"
                                 >Job Title</label
                             >
-                            <input
-                                type="text"
+                            <input 
                                 class="border border-gray-200 rounded p-2 w-full"
+                                type="text"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{ old('title') }}"
                             />
 
                             @error('title')
@@ -57,6 +58,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
+                                value="{{ old('location') }}"
+                                
                             />
 
                             @error('location')
@@ -72,6 +75,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value="{{ old('email') }}"
                             />
 
                             @error('email')
@@ -90,6 +94,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value="{{ old('website') }}"
                             />
 
                              @error('website')
@@ -107,6 +112,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                                value="{{ old('tags') }}"
                             />
 
                             @error('tags')
@@ -137,7 +143,8 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
-                            ></textarea>
+                                
+                            >{{ old('description')}}</textarea>
 
                             @error('description')
                                  <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
