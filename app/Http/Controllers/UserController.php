a8a4ebdb5dcf7c85204e01da0788b5f886eed7ea
nemanjakovi <nemanjakovi90@gmail.com>
2022-12-01 14:31:40 +0100

    :User Registration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+
+class UserController extends Controller
+{
+
+    // Show Register Create Form
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    // Create New User
+    public function store(Request $request)
+    {
+        $formFilds = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6']
+        ]);
+
+        //Hash Password
+        $formFilds['password'] = bcrypt($formFilds['password']);
+
+        //Create User
+        $user = User::create($formFilds);
+
+        // Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ListingControler;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
 
 
@@ -19,21 +19,21 @@ use App\Http\Controllers\ListingController;
 */
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
-//Show Create Form
+// Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
-
-//Store Listing Data
+// Store Listing Data
 Route::POST('/listings', [ListingController::class, 'store']);
-//Show Edit Form 
+// Show Edit Form 
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
-
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
-
-//Delete Listing
+// Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
-
-
-
-//Single Listing
+// Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+//Show Register Create Form
+Route::get('/register', [UserController::class, 'create']);
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
